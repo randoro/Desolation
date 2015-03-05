4a97460ecbc90df66bebae76e8730ee3e6d690ea
Randoro <anton.arnoldsson@hotmail.com>
2015-03-05 23:54:20 +0100

    adding back

---
+++ Desolation/Desolation/ChunkManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Desolation
+{
+    class ChunkManager
+    {
+
+        List<Region> regionFiles;
+
+        public ChunkManager()
+        {
+            regionFiles = new List<Region>();
+        }
+
+
+
+
+    }
+}
