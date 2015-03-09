d275aea438a584a427b4e327f3b585ebec15d170
Randoro <anton.arnoldsson@hotmail.com>
2015-03-09 19:59:21 +0100

    Global

--- Desolation/Desolation/Globals.cs
+++ Desolation/Desolation/Globals.cs
@@ -9,7 +9,6 @@ namespace Desolation
     public enum TagID { End, Byte, Short, Int, Long, Float, Double, ByteArray, String, List, Compound, IntArray };
     static class Globals
     {
-
         public static readonly String gamePath = AppDomain.CurrentDomain.BaseDirectory;
         public static readonly int ticksPerChunkLoad = 10000000;
     }
