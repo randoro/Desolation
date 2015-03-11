7d012ffb88c0083c34fe439bf9e64ea4d25d04fc
lokatt93 <datajocke93@gmail.com>
2015-03-11 13:33:30 +0100

    gjorde en Zombie
    braiiiiiiins

---
+++ Desolation/Desolation/Zombie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Desolation
+{
+    class Zombie :Entity
+    {
+        public Zombie(Texture2D sheet, Rectangle rect, Vector2 pos)
+
+            : base(sheet, rect, pos)
+        {
+
+        }
+
+    }
+}
