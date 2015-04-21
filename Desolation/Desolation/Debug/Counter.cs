97cb908e6202e7561ed3e2d848bb9a8bd8b10e26
Randoro-Skola <anton.arnoldsson@hotmail.com>
2015-04-21 12:34:47 +0200

    test

---
+++ Desolation/Desolation/Debug/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Desolation
+{
+    class Counter
+    {
+
+        public int frameRate = 0;
+        int frameCounter = 0;
+        TimeSpan elapsedTime = TimeSpan.Zero;
+
+        public Counter()
+        {
+
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            elapsedTime += gameTime.ElapsedGameTime;
+
+            if (elapsedTime > TimeSpan.FromSeconds(1))
+            {
+                elapsedTime -= TimeSpan.FromSeconds(1);
+                frameRate = frameCounter;
+                frameCounter = 0;
+            }
+        }
+
+
+        public void increaseCounter()
+        {
+            frameCounter++;
+        }
+    }
+}
