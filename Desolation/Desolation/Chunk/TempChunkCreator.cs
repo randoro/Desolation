73207930d7ead7ffd5fa8b172918171462b164b4
Randoro <anton.arnoldsson@hotmail.com>
2015-04-22 23:20:20 +0200

    fix

--- Desolation/Desolation/Chunk/TempChunkCreator.cs
+++ Desolation/Desolation/Chunk/TempChunkCreator.cs
@@ -103,7 +103,7 @@ namespace Desolation
                 else
                 {
 
-                    objects[j] = (byte)1;
+                    objects[j] = (byte)0;
                 }
             }
 
