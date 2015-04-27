f6e776fa94d571735d9f2ac6379b1f9484c7c042
hamdiieko <hamdimaraning@hotmail.com>
2015-04-27 13:35:59 +0200

    particle klass

---
+++ Desolation/Desolation/Particle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Desolation
+{
+    public class Particle
+    {
+        public Texture2D text { get; set; }
+        public Vector2 pos { get; set; }
+        public Vector2 vel { get; set; }
+        public float angle { get; set; }
+        public float angularVel { get; set; }
+        public Color Color { get; set; }
+        public float size { get; set; }
+        public int TTL { get; set; } //TTL = time to live för partiklarna 
+
+        public Particle(Texture2D text, Vector2 pos, Vector2 vel, float angle, float angularVel, Color Color, float size, int ttl)
+        {
+            this.text = text;
+            this.pos = pos;
+            this.vel = vel;
+            this.angle = angle;
+            this.angularVel = angularVel;
+            this.Color = Color;
+            this.size = size;
+            this.TTL = ttl;
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
