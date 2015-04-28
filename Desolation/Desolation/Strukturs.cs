5a3c6069ff75569fc000c30f17e6604bb0d8594e
lokatt93 <datajocke93@gmail.com>
2015-04-28 11:31:19 +0200

    Strukturklass

---
+++ Desolation/Desolation/Strukturs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Desolation
+{
+    class Strukturs
+    {
+        public Strukturs()
+        {
+
+        }
+        public void Update(GameTime gameTime)
+        { 
+
+        }
+        public void Hus()
+        {
+            
+                int startpos = Globals.rand.Next(0, 255);
+                int chunkNr = Game1.player.getCurrentChunkNrInArray(Globals.playerPos, Globals.playerPos);
+                Chunk tempChunk = ChunkManager.chunkArray[chunkNr];
+                if (tempChunk != null)
+                {
+
+                    #region horesentel
+                    tempChunk.objects[startpos] = 1;
+                    for (int i = 0; i < Globals.rand.Next(15, 18); i++)
+                    {
+                        bool fis = true;
+                        int test5 = startpos + i;
+                        if (test5 < (tempChunk.objects.Length) && test5 >= 0 && fis)
+                        {
+                            tempChunk = ChunkManager.chunkArray[chunkNr];
+                            tempChunk.objects[test5] = 1;
+                        }
+
+
+                    }
+                    #endregion
+                    for (int i = 0; i < Globals.rand.Next(5, 15); i++)
+                    {
+                        bool fis = true;
+                        int test5 = startpos + ((i * 16) + 15);
+                        if (test5 < (tempChunk.objects.Length) && test5 >= 0 && fis)
+                        {
+                            tempChunk = ChunkManager.chunkArray[chunkNr];
+                            tempChunk.objects[test5] = 1;
+                        }
+
+                    }
+
+                }
+            
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
