0142fd9372d47ff54aa2707d34c5dc203eadf187
lokatt93 <datajocke93@gmail.com>
2015-04-28 12:52:03 +0200

    hus

--- Desolation/Desolation/Strukturs.cs
+++ Desolation/Desolation/Strukturs.cs
@@ -19,47 +19,73 @@ namespace Desolation
 
         }
         public void Update(GameTime gameTime)
-        { 
+        {
 
         }
+        public void Hustest()
+        {
+            int startpos = Globals.rand.Next(0, 255);
+
+            int chunkNr = Game1.player.getCurrentChunkNrInArray(Globals.playerPos, Globals.playerPos);
+            Chunk tempChunk = ChunkManager.chunkArray[chunkNr];
+            if (tempChunk != null)
+            {
+
+                for (int i = 0; i < Globals.rand.Next(100, 255); i++)
+                {
+                    if (chunkNr != -1)
+                    {
+                        //kålla för varje blåck om det är i ny chunk 
+                        tempChunk = ChunkManager.chunkArray[chunkNr];
+
+                        Vector2 vec = new Vector2((i + startpos) / 16, (i + startpos) % 16);
+
+                        chunkNr = Game1.player.getCurrentChunkNrInArray(vec, Globals.playerPos);
+
+                        tempChunk.objects[i] = 1;
+                    }
+                }
+            }
+        }
+
         public void Hus()
         {
-            
-                int startpos = Globals.rand.Next(0, 255);
-                int chunkNr = Game1.player.getCurrentChunkNrInArray(Globals.playerPos, Globals.playerPos);
-                Chunk tempChunk = ChunkManager.chunkArray[chunkNr];
-                if (tempChunk != null)
+
+            int startpos = Globals.rand.Next(0, 255);
+            int chunkNr = Game1.player.getCurrentChunkNrInArray(Globals.playerPos, Globals.playerPos);
+            Chunk tempChunk = ChunkManager.chunkArray[chunkNr];
+            if (tempChunk != null)
+            {
+
+                #region horesentel
+                tempChunk.objects[startpos] = 1;
+                for (int i = 0; i < Globals.rand.Next(15, 18); i++)
                 {
-
-                    #region horesentel
-                    tempChunk.objects[startpos] = 1;
-                    for (int i = 0; i < Globals.rand.Next(15, 18); i++)
+                    bool fis = true;
+                    int test5 = startpos + i;
+                    if (test5 < (tempChunk.objects.Length) && test5 >= 0 && fis)
                     {
-                        bool fis = true;
-                        int test5 = startpos + i;
-                        if (test5 < (tempChunk.objects.Length) && test5 >= 0 && fis)
-                        {
-                            tempChunk = ChunkManager.chunkArray[chunkNr];
-                            tempChunk.objects[test5] = 1;
-                        }
-
-
+                        tempChunk = ChunkManager.chunkArray[chunkNr];
+                        tempChunk.objects[test5] = 1;
                     }
-                    #endregion
-                    for (int i = 0; i < Globals.rand.Next(5, 15); i++)
-                    {
-                        bool fis = true;
-                        int test5 = startpos + ((i * 16) + 15);
-                        if (test5 < (tempChunk.objects.Length) && test5 >= 0 && fis)
-                        {
-                            tempChunk = ChunkManager.chunkArray[chunkNr];
-                            tempChunk.objects[test5] = 1;
-                        }
 
+
+                }
+                #endregion
+                for (int i = 0; i < Globals.rand.Next(5, 15); i++)
+                {
+                    bool fis = true;
+                    int test5 = startpos + ((i * 16) + 15);
+                    if (test5 < (tempChunk.objects.Length) && test5 >= 0 && fis)
+                    {
+                        tempChunk = ChunkManager.chunkArray[chunkNr];
+                        tempChunk.objects[test5] = 1;
                     }
 
                 }
-            
+
+            }
+
 
         }
         public void Draw(SpriteBatch spriteBatch)
