379273f2d39ce24301fa6eccb54dd63c4f179eda
Randoro <anton.arnoldsson@hotmail.com>
2015-05-05 15:04:21 +0200

    stuff

--- Desolation/Desolation/Chunk/Structure.cs
+++ Desolation/Desolation/Chunk/Structure.cs
@@ -29,7 +29,7 @@ namespace Desolation
             newRoom.generateRoom();
             ChunkManager.roomList.Add(newRoom);
 
-            Room newRoom2 = new Room(structureCenterBlockX * 16 - 64 + 200, structureCenterBlockY * 16 - 32 + 200, 128, 128, structureID);
+            Room newRoom2 = new Room(structureCenterBlockX * 16 - 64 + 192, structureCenterBlockY * 16 - 32 + 192, 128, 128, structureID);
             newRoom2.generateRoom();
             ChunkManager.roomList.Add(newRoom2);
 
